85fe49df3bb4178bbbfe3b372ad94567bd67c37e
Evan Deaubl <evan@deaubl.name>
2019-06-09 14:09:03 -0700

    Add base SwiftUI/Core Data/CloudKit project

---
+++ TodoNG/TodoNG/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TodoNG
+//
+//  Created by Evan Deaubl on 6/9/19.
+//  Copyright © 2019 Tic Tac Code, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
